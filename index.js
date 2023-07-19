36b270e67f75285d31aa578c8d0f3f6b8cf97472
Monil Sharma <68782355+monil-sharma@users.noreply.github.com>
2023-07-19 20:00:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+dotenv.config();
+
+mongoose.connect(process.env.URL, () => {
+  console.log("Connection to MongoDB established");
+});
+mongoose.set("strictQuery", true);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Server started on 8800.");
+});
