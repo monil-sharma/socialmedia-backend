dc6f271def954983a5fb0c568e9f8aa39d47ef7f
Monil Sharma <68782355+monil-sharma@users.noreply.github.com>
2023-07-19 21:03:41 +0530

    get and del user added

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const { json } = require("express");
 
 //Update user
 router.put("/:id", async (req, res) => {
-  if (req.body.userID === req.params.id || req.user.isAdmin) {
+  if (req.body.userID === req.params.id || req.body.isAdmin) {
     //update password then->
     if (req.body.password) {
       try {
@@ -29,7 +30,32 @@ router.put("/:id", async (req, res) => {
 });
 
 //Delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userID === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    res.status(405).json("You can delete YOUR account only!");
+  }
+});
+
 //Get a user
+router.get("/:id", async (req, res) => {
+  if (req.body.userID === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findById(req.params.id);
+      const { password, createdAt, updatedAt, __v, ...other } = user._doc; //here we filter out which fields we will display
+      res.status(200).json(other);
+    } catch (err) {
+      return res(500).json(err);
+    }
+  }
+});
+
 //Follow a user
 //Unfollow a user
 module.exports = router;
